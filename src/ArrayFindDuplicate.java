f83698ab5f35e6adb0c9c091e9c7a59bdd957ed3
ashishchoudhary <aashish.choudhary1@gmail.com>
2019-12-14 12:50:59 +0530

    added code to find Duplicate Elements in the given Array

---
+++ src/ArrayFindDuplicate.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class ArrayFindDuplicate {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 5, 6, 2, 2};
+        Set<Integer> set = new HashSet<>(array.length);
+        System.out.println("Duplicate Elements are!");
+        for (int i = 0; i < array.length; i++) {
+            if (set.add(array[i]) == false) {
+                System.out.println(array[i]);
+            }
+        }
+    }
+}
