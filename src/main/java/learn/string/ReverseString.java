900ebff59615ce2bf67f87fda2cc1ddc03e63997
ashishchoudhary <aashish.choudhary1@gmail.com>
2019-12-14 21:33:52 +0530

    added code to ReverseString

--- src/main/java/learn/string/ReverseString.java
+++ src/main/java/learn/string/ReverseString.java
@@ -4,10 +4,14 @@ public class ReverseString {
     public static void main(String[] args) {
         String string = "pivotal gemfire";
         final char[] chars = string.toCharArray();
+        System.out.println("using char array");
         for (int i = chars.length - 1; i >= 0; i--) {
-            System.out.println(chars[i]);
+            System.out.print(chars[i]);
         }
-
-
+        System.out.println("");
+        System.out.println("using stringbuilder");
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(string);
+        System.out.println(stringBuilder.reverse());
     }
 }
